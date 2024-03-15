79b345b7ef868addb82a83e8a066a720b90b8b9e
andy <weijung0923@gmail.com>
2024-03-15 15:58:27 +0800

    更新about

--- src/views/frontend/AboutView.vue
+++ src/views/frontend/AboutView.vue
@@ -1,63 +1,34 @@
 <template lang="">
     <ImageSectionVue title='關於我們' background-image='url(https://hexschool-api.s3.us-west-2.amazonaws.com/custom/U8YjSv6kB6Hfx6uYp42LSDTcZ6fPUvRSqzIhvWeaPGjyHqjlLFV7FSKJZijzQEFsRsgbjPfuK7M4Ai7Pvk15Gz3gyHFEa2O2lEQ0HxXysp1GEQH0MQsuNdMkXcCCvcy9.jpg)'/>
 
-
-    <!-- <section class="part intro">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="img-wrap">
-              <img
-                data-aos="fade-up"
-                class="img-fluid"
-                src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/i4mXi8E6EwKBjH7nfnRF1vrXrLmVsOzdNsig8d72cNVgAc103KqIbTl4uFfoCDXK8tQpap096VZBxar7fforwsyNivsGoajnTrvoTxgDdEhtXIgcgiLFCqyqPVruDbie.png"
-              >
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div
-              class="txt-wrap p-3"
-              data-aos="fade-down"
-            >
-              <h2 class="my-3">
-                以福音為中心
-              </h2>
-              <p>基督與福音是一個整體，是信徒敬拜的對象，也是更新生命的動力。 因此我們渴望建立『以福音為中心』的教會， 讓人們能夠呼喊——福音的奇妙與偉大！</p>
-              <p>
-                想要⼀間以福⾳為中⼼的教會，是受已故紐約救贖主⻑⽼教會的提摩太凱勒牧師所提 出的運動影響。 ⽽不是在說這是⼀間『傳福⾳』為中⼼的教會，雖然傳福⾳對教會來說是最⾃然的 事。也不是在說這是⼀間『只講恩典』的教會，雖然因著基督信徒活在恩典裡。 這個理念不容易⼀下⼦理解，我這樣說：⼀個⼈信耶穌後，他的⽣命會是如何？是如 同受洗的那天，還是不斷更新？（如果是因著福⾳應當不斷更新）。
-              </p>
-              <p>
-                然⽽，如果我們使⼈信主只是把⼀個⼈從傳統宗教改成信耶穌，這樣的⽅法無法使⼈ 持續更新。⽽是，信耶穌和明⽩福⾳是⼀個整體。⼈信了耶穌就要持續明⽩福⾳來更 新，另⼀⽅⾯⼈是因為明⽩福⾳⽽信耶穌，因此福⾳就成為這間教會最⾸要的事。 信徒明⽩福⾳到道裡將會歡喜快樂接受⾃⼰是罪⼈的事實，更要歡喜快樂基督的救恩 ⼤過這⼀切。
-              </p>
-              <p>
-                以此改變內⼼思維的⽅式，因著救恩得來的恩典將偶像換為基督、將交 換條件換為因著愛、將孤兒的⼼換為兒⼥的⼼、將世界的滿⾜換為基督為滿⾜以及將 ⾃我中⼼換為基督為中⼼，這就是以福⾳為中⼼的教會特徵。
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
-
 		<section class="part">
-			<div class="container gaspel-center">
+			<div class="container-md gaspel-center">
+				
 				<div class="row flex-row-reverse">
+					<!-- <div class="col-xl-1 d-flex align-items-center"></div> -->
 					<div 
-						class="col-xl-7"
-						data-aos="fade-down" 	
+						class="col-xl-4 pastor"
+						data-aos="fade-down"
 					>
-						<img  style="width: 100%; height:422px; object-fit: cover" src="https://cdn.pixabay.com/photo/2015/03/30/14/35/love-699480_1280.jpg" alt="picture">
+						<img class="pastor" src="@/assets/images/IMG_2646.jpg" alt="picture">
 					</div>
-
-					<div class="col-xl-5 d-flex align-items-center">
+					
+					<div class="col-xl-8 d-flex align-items-center">
 						<div class="gaspel-center-content" data-aos="fade-up">
-							<div class="subtip">GOSPEL</div>
+							<!-- <div class="subtip">PASTOR</div> -->
 							<h2>
-								<span>以福音為中心的教會</span>
+								<span>教會牧師 – 高文聰</span>
 							</h2>
 							<!-- <h2>
 								<span>是什麼樣？</span>
 							</h2> -->
-							<p>福音就是基督為自己百姓做成的救恩，教會以此為中心不斷發現福音的恩典運用在生命上，離開以行為換取祝福的宗教模式。</p>
+							<p>2021年12月，高文聰牧師本於對福音的熱情開拓磐石之心教會，期望能在新興教會與傳統教會中取得平衡。在教會中首要任務是講明福音的本質，使信徒得著福音的好處，實踐在生活各個層面中。在教導上，強調聖經為教會的權威。致力教導聖經神學、聖約神學、以基督為中心的講道、福音轉化、西敏信條…等。每周在線上帶領以救贖歷史框架查考聖經，使信徒進入整本聖經的偉大救贖圖畫中，等候末世最後的成全。</p>
+							
+							<p>2019年6月 台北改革宗神學院道學碩士畢業</p>
+							<p>2019年7月 CITY to CITY TAIWAN 植堂者訓練結業</p>
+							<p>2020年4月 台中烈火教會擔任傳道人</p>
+							<p>2020年9月 晨曦福音戒毒學院兼任教師</p>
+							<p>2021年12月 開拓磐石之心教會</p>
 							<!-- <img src="https://iamamberhh.github.io/sollys/assets/images/dream-1.png" alt="slogan"> -->
 						</div>
 					</div>
@@ -72,19 +43,24 @@
 						class="col-xl-7"
 						data-aos="fade-down" 	
 					>
-						<img style="width: 100%; height:422px; object-fit: cover" src="https://cdn.pixabay.com/photo/2016/04/16/18/00/scales-1333455_1280.jpg" alt="picture">
+						<img style="width: 100%; height:422px; object-fit: cover" src="@/assets/images/IMG_2647.jpg" alt="picture">
 					</div>
 
 					<div class="col-xl-5 d-flex align-items-center">
 						<div class="gaspel-center-content" data-aos="fade-up">
-							<div class="subtip">BALANCE</div>
+							<!-- <div class="subtip">BALANCE</div> -->
 							<h2>
-								<span>兼容平衡的教會</span>
+								<span>教會監督-黃宗儀</span>
 							</h2>
 							<!-- <h2>
 								<span>是什麼樣？</span>
 							</h2> -->
-							<p>崇拜、團契與使命是健康教會均衡發展的三要素，避免教會成為神學院或內聚的團體。</p>
+							<p>黃宗儀牧師是華神校友，曾在客西馬尼行道會，台中精誠教會（開拓），費城台福教會及台中福音基督教會牧會，黃宗儀牧師在獲得英國牛津宣教研究中心的哲學博士之後，曾在中台神學院專任教學，現為黃藥師的福音神學教室的創辦人，致力於為教會客製化神學課程，把神學帶進教會，將福音神學的核心真理透過解經，神學及實踐的課程轉化生命。基督教今日報專訪：擁叛逆靈魂　從小提琴手、牙醫到牧師：盼不要自居為「比較好的罪人」
+							</p>
+							<p style="text-align: right">
+								<button type="button" class="btn btn-success" onclick="location.href='https://www.facebook.com/HYSClassroom?locale=zh_TW'">福音神學教室</button>
+								<button type="button" class="btn btn-primary" style="margin-left: 10px" onclick="location.href='https://tinyurl.com/bfmnxjwz'">比較好的罪人</button>
+							</p>
 							<!-- <img src="https://iamamberhh.github.io/sollys/assets/images/dream-1.png" alt="slogan"> -->
 						</div>
 					</div>
@@ -92,96 +68,6 @@
 			</div>
 		</section>
 
-		<section class="part">
-			<div class="container gaspel-center">
-				<div class="row flex-row-reverse">
-					<div 
-						class="col-xl-7"
-						data-aos="fade-down" 	
-					>
-						<img  style="width: 100%; height:422px; object-fit: cover" src="https://cdn.pixabay.com/photo/2016/11/21/16/09/bible-1846174_1280.jpg" alt="picture">
-					</div>
-
-					<div class="col-xl-5 d-flex align-items-center">
-						<div class="gaspel-center-content" data-aos="fade-up">
-							<div class="subtip">BIBLE</div>
-							<h2>
-								<span>學習聖經的教會</span>
-							</h2>
-							<!-- <h2>
-								<span>是什麼樣？</span>
-							</h2> -->
-							<p>聖經是上帝所啟示的生命之道，因此教會有責任教導信徒聖經真理，指出聖經前後一致的焦點是基督，以及祂如何因為愛所成就的事。</p>
-							<!-- <img src="https://iamamberhh.github.io/sollys/assets/images/dream-1.png" alt="slogan"> -->
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<!-- <section class="part">
-			<div class="container balance-church">
-				<div class="row">
-					<div class="col-xl-6">
-						<div class="about-content" data-aos="fade-up">
-							<div class="subtip">ABOUT SOLLYS</div>
-							<h2>
-								<span>學習聖經的教會</span>
-							</h2>
-							<p>Sollys取自丹麥文，是「陽光」之意，丹麥是全球最快樂的國家，因此，工作室的宗旨，就是想把快樂聖地丹麥的陽光帶進每個屋主的家。</p>
-							<img src="https://iamamberhh.github.io/sollys/assets/images/sunset.png" alt="picture" class="d-md-none d-lg-block deco-sunset">
-						</div>
-					</div>
-
-					<div class="col-xl-1"></div>
-
-					<div class="col-xl-4">
-						<div class="deco-block" data-aos="fade-down" ></div>
-					</div>
-
-				</div>
-			</div>
-
-		</section> -->
-
-
-    <!-- <section class="part">
-			<div class="container">
-        <div class="row flex-row-reverse">
-					<div
-						class="col-md-6 d-flex flex-column justify-content-center p-4"
-						data-aos="fade-up" 
-					>
-						<h2 class="text-center fw-bolder mb-3">兼容平衡的教會</h2>
-						<p class="text-start px-4 mb-0">
-							這三⼗年間教會⽣態有巨⼤的變化，造成『傳統教會』與『新興教會』有著不同的⽬ 的與形式。 因此想要⼀間兼容平衡的教會，前提是把福⾳真理看得夠清晰，便能幫助我們有正確 地學習『新興教會』對國度的熱情，與『傳統教會』的信仰基礎。 網站資料 2 因此如圖所⽰，福⾳仍舊是教會中最⾸要的事，構成了教會的敬拜、團契與使命的核 ⼼。⽽這三個⽅向缺乏⼀個就會讓教會像是『神學院』、『同溫群體』或『事⼯機 構』。雖然⼀間健全的教會⾃然會有這些功能，卻不能本末倒置。唯有教會本⾝聚焦 於福⾳，才能夠幫助群體在三個⽅向平衡的發展。
-						</p>
-					</div>
-					<div class="col-md-6 p-4" data-aos="zoom-in">
-						<img class="img-fluid" src="@/assets/images/k.jpg" alt="about_img">
-					</div>
-				</div>
-			</div>
-    </section>
-
-		<section class="part">
-			<div class="container">
-        <div class="row">
-					<div
-						class="col-md-6 d-flex flex-column justify-content-center p-4"
-						data-aos="fade-down" 
-					>
-						<h2 class="text-center fw-bolder mb-3">學習聖經的教會</h2>
-						<p class="text-start px-4 mb-0">
-							教會是基督徒不能或缺的需要，信徒可以⾃由地在各種恩賜上發 展。然⽽對聖經的學習並不是個⼈⾃由，⽽是信徒的⽣命成⻑必要的⼀環。 有的信徒會認書經⾃⼰閱讀就可以了，不需要有⼈教導。 或者是沒有⼈能夠完全無誤 地解釋聖經，因此對於學習有種⽝奴⼼態。其實這都是⾼舉了⼈的獨⽴性，忽略上帝 把⼈須明⽩的話都寫在聖經裡。並且信徒會⾃然地發展出⾃⼰的⼀套『神學模式』， ⽤⾃⼰的⽅式在⽣活。所以想要⼀間學習聖經的教會的原因是，相信聖經是上帝無誤的話。耶是教會的最⾼ 權威。另⼀⽅⾯，幫助信徒閱讀聖經。在聖經中思考各類⽣命主題，並在基督的救恩 中找到盼望與更新。
-						</p>
-					</div>
-					<div class="col-md-6 p-4" data-aos="zoom-in">
-						<img class="img-fluid" src="@/assets/images/h.jpg" alt="about_img">
-					</div>
-				</div>
-			</div>
-    </section> -->
 
 		<section class="part pt-3">
       <div class="img-wrap d-none d-md-block">
@@ -209,7 +95,7 @@ import ImageSectionVue from '@/components/Frontend/Layout/ImageSection.vue';
 // import QAndASectionVue from '../components/QAndASection.vue';
 </script>
 <style lang="scss" scoped>
-*{
+* {
 	position: relative;
 }
 
@@ -233,60 +119,62 @@ import ImageSectionVue from '@/components/Frontend/Layout/ImageSection.vue';
 	z-index: 1;
 }
 
-.gaspel-center{
-	.gaspel-center-content{
+.gaspel-center {
+	.gaspel-center-content {
 		margin-left: 20px;
 
-		h2{
+		h2 {
 			font-size: 40px;
 			font-weight: bold;
 			margin-bottom: 20px;
-			
+
 		}
 
-		span{
+		/* span {
 			border-bottom: 1px solid #4F4F51;
-		}
+		} */
 
-		.subtip{
+		.subtip {
 			margin-bottom: 10px;
 		}
 
-		p{
+		p {
 			margin: 25px 0 20px;
 			line-height: 2;
 			letter-spacing: 3.5px;
 		}
 	}
 
-	img{
-		width: 100%;
+	img {
+		height: 550px;
+		object-fit: cover;
 	}
 }
 
 
-.balance-church{
+.balance-church {
 
-	h2{
+	h2 {
 		font-size: 40px;
 		font-weight: bold;
 		margin-bottom: 20px;
-		
+
 	}
 
-	span{
+	span {
 		border-bottom: 1px solid #4F4F51;
 	}
 
-	.subtip{
+	.subtip {
 		margin-bottom: 10px;
 	}
 
-	p{
+	p {
 		margin: 25px 0 20px;
 		line-height: 2.25;
 		letter-spacing: 5px;
 	}
+
 	.deco-block {
 		width: 100%;
 		/* margin-right: 12%; */
@@ -296,7 +184,7 @@ import ImageSectionVue from '@/components/Frontend/Layout/ImageSection.vue';
 		background-position: center;
 		background-size: cover;
 	}
-	
+
 	.deco-block::after {
 		content: "";
 		position: absolute;
@@ -312,18 +200,29 @@ import ImageSectionVue from '@/components/Frontend/Layout/ImageSection.vue';
 		position: absolute;
 		width: 180px;
 		height: 180px;
-    right: 0px;
+		right: 0px;
 	}
 }
 
+/* .pastor {
+	width: 100%;
+	object-fit: cover
+} */
 
-
-@media(max-width:1200px){
-  .gaspel-center{
-		.gaspel-center-content{
+@media(max-width:1200px) {
+	.gaspel-center {
+		.gaspel-center-content {
 			margin-top: 50px;
 		}
 	}
-}
 
-</style>
+	.gaspel-center {
+		.pastor{
+			text-align: center;
+		}
+		img {
+			width: 400px;
+		}
+	}
+}
+</style>import { styleType } from 'element-plus/es/components/table-v2/src/common';
